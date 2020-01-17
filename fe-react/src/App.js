fdc07858e7c7c78f9245f86463134873cec45a7c
Loc Thanh <locnt19@gmail.com>
2020-01-17 09:07:17 +0700

    🆕 Use Boostrap and add TopMenu

--- fe-react/src/App.js
+++ fe-react/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import TopMenu from './components/TopMenu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TopMenu />
       </div>
     );
   }
---
+++ fe-react/src/components/TopMenu.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
+const TopMenu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Simple</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/profiles/">Profiles</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>username</NavbarText>
+          <NavLink href="/login/">Login</NavLink>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default TopMenu;
