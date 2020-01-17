dc8b9a2b7a8e05ca123a936898d42e2639134e6e
Loc Thanh <locnt19@gmail.com>
2020-01-17 10:20:49 +0700

    🆕 Add Products page and load data

--- fe-react/src/App.js
+++ fe-react/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import TopMenu from './components/TopMenu';
+import Products from './pages/Products';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <TopMenu />
+          <Route path="/products" component={Products} />
         </div>
       </Router>
     );
