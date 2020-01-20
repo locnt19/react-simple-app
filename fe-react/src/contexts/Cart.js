899e4f601a59ab95983f8e1876f94e3eb5e3f1d3
Loc Thanh <locnt19@gmail.com>
2020-01-20 10:34:30 +0700

    🆙 [ContextAPI Cart] - Add event `removeFromCart`

--- fe-react/src/contexts/Cart.js
+++ fe-react/src/contexts/Cart.js
@@ -11,20 +11,37 @@ export class CartProvider extends Component {
     }
 
     this.addToCart = this.addToCart.bind(this);
+    this.removeFromCart = this.removeFromCart.bind(this);
   }
 
   addToCart(product) {
     this.setState({
       cartItems: this.state.cartItems.concat(product)
     });
-    console.log('Add to cart successful!', this.state.cartItems);
+    alert('Add to cart successful!');
+    console.log('Add product to cart.');
+  }
+
+  removeFromCart(index) {
+    const { cartItems } = this.state;
+    this.setState({
+      cartItems: [
+        ...cartItems.slice(0, index),
+        ...cartItems.slice(index + 1)
+      ]
+    });
+    alert(`Remove product ${cartItems[index].name} from cart successful!`);
+    console.log('Remove product from cart.');
   }
 
   render() {
+    console.log('Rendering...')
+    console.log('state.cartItems:', this.state.cartItems);
     return (
       <CartContext.Provider value={{
         cartItems: this.state.cartItems,
-        addToCart: this.addToCart
+        addToCart: this.addToCart,
+        removeFromCart: this.removeFromCart
       }}>
         {
           this.props.children
--- fe-react/src/pages/Cart.js
+++ fe-react/src/pages/Cart.js
@@ -13,13 +13,14 @@ class Cart extends Component {
         <Row>
           <CartContext.Consumer>
             {
-              ({ cartItems }) => (
+              ({ cartItems, removeFromCart }) => (
                 cartItems.map((product, index) => (
                   <Col xs="12" key={index} className="mt-4">
                     <img src={product.imageURL} alt={product.name} />
                     <h2>{product.name}</h2>
                     <h4>{product.producer}</h4>
                     <p>{product.description}</p>
+                    <button className="btn btn-danger" onClick={() => removeFromCart(index)}>Remove</button>
                   </Col>
                 ))
               )
