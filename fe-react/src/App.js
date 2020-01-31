5ef3e6d1cb854f0bb85e54f8e7d674aa23785685
Loc Thanh <locnt19@gmail.com>
2020-01-31 15:46:38 +0700

    🆙 Add route for Login page

--- fe-react/src/App.js
+++ fe-react/src/App.js
@@ -4,31 +4,36 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import { CartProvider } from './contexts/Cart';
+import { InOutProvider } from './contexts/InOut';
 import TopMenu from './components/TopMenu';
 
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
 import NotFound from './pages/404';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
     return (
       <CartProvider>
-        <Router>
-          <div className="App">
-            <TopMenu />
-            <div className="App-content">
-              <Switch>
-                <Route path="/" exact><h1>Home page</h1></Route>
-                <Route path="/products" exact component={Products}/>
-                <Route path="/product/:id" exact component={ProductDetail}/>
-                <Route path="/cart" exact component={Cart}/>
-                <Route path="*" component={NotFound} />
-              </Switch>
+        <InOutProvider>
+          <Router>
+            <div className="App">
+              <TopMenu />
+              <div className="App-content">
+                <Switch>
+                  <Route path="/" exact><h1>Home page</h1></Route>
+                  <Route path="/products" exact component={Products} />
+                  <Route path="/product/:id" exact component={ProductDetail} />
+                  <Route path="/cart" exact component={Cart} />
+                  <Route path="/login" exact component={Login} />
+                  <Route path="*" component={NotFound} />
+                </Switch>
+              </div>
             </div>
-          </div>
-        </Router>
+          </Router>
+        </InOutProvider>
       </CartProvider>
     );
   }
